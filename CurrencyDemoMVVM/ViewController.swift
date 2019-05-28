5848d3f3185d0e1a1c8e00215c84e3dbb71f5ce5
Salma Atef <salma.atef.ebrahim@gmail.com>
2019-05-28 12:59:01 +0200

    Initial Commit

---
+++ CurrencyDemoMVVM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurrencyDemoMVVM
+//
+//  Created by Salma Atef Saeid on 5/28/19.
+//  Copyright © 2019 Salma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
